cdcb7abf8d2034bcbfc2d6183a19ac57a87c9b22
Diego Isaías <dicruzcar.dev@gmail.com>
2020-04-30 21:35:31 -0500

    Controls now work more precisely by frame update

--- pygame_entities/game.py
+++ pygame_entities/game.py
@@ -24,21 +24,31 @@ class Game():
         self.snake_value = snake_value
         self.food_value = food_value
         self.food_color = (22, 160, 133)
+        self.counter = 0
 
         #Game attributes
         self.restart_key = "SPACE"
-        self.true_game_frames = 6
-        self.reaction_frames = 60
-        self.true_frames_and_reaction_coeficent = int(self.reaction_frames/self.true_game_frames)
+        self.game_frames = 6
+        self.ui_frames = 144
+        self.frames_per_action_render = int(self.ui_frames/self.game_frames)
         self.win = False
         self.h1_font = pygame.font.Font(self.fonts("Blinker-Regular.ttf"), 40)
         self.h2_font = pygame.font.Font(self.fonts("Blinker-Regular.ttf"), 20)
         self.win_text = "¡Felicidades!, ganaste"
-        self.win_surface = self.h1_font.render(self.win_text, True, (110,193,229))
+        self.win_surface = self.h1_font.render(
+            self.win_text, True, (110, 193, 229)
+        )
+
         self.dead_text = "Has muerto :("
-        self.dead_surface = self.h1_font.render(self.dead_text, True, (228,82,50))
+        self.dead_surface = self.h1_font.render(
+            self.dead_text, True, (228, 82, 50)
+        )
+        
         self.restart_text = f"Presiona [{self.restart_key}] para reiniciar"
-        self.restart_surface = self.h2_font.render(self.restart_text, True, (0,0,0))
+        self.restart_surface = self.h2_font.render(
+            self.restart_text, True, (0, 0, 0)
+        )
+        
         self.game_text_margins = 10
 
         #World objects
@@ -54,36 +64,21 @@ class Game():
     def main_thread(self):
 
         clock = pygame.time.Clock()
-        counter = 0
-        key_buffer = [self.snake.direction]
+        self.counter = 0
 
         while True:
 
-            for events in pygame.event.get():
-                self.events_manager(events.type)
+            for event in pygame.event.get():
+                self.events_manager(event)
 
-            key_buffer = self.keyboard_events(key_buffer)
-
-            if counter == self.true_frames_and_reaction_coeficent:
-                key_buffer = self.keyboard_buffer(key_buffer)
+            if self.counter == self.frames_per_action_render:
                 self.load_world()
-                counter = 0
+                self.counter = 0
 
-            clock.tick(self.reaction_frames)
-            counter = counter + 1
+            clock.tick(self.ui_frames)
+            self.counter = self.counter + 1
 
         return 0
-
-    def load_image(self, filename, transparent=False):
-        try: image = pygame.image.load(filename) #Try to load the image
-        except pygame.error:                     #Manage the exception
-            print("Error al cargar la Imagen", pygame.get_error())
-            raise (SystemExit)
-        image = image.convert()                 #Convert to pygame friendly format
-        if transparent:                         #Manage transparency
-                color = image.get_at((0,0))
-                image.set_colorkey(color, pygame.RLEACCEL)
-        return image
 
     def fonts(self, name):
         base_path = Path.cwd()
@@ -91,11 +86,25 @@ class Game():
         return final_path
 
     def events_manager(self, event):
-            if event == pygame.QUIT:
-                sys.exit(0)
-
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                self.snake.change_dir(self.snake.get_north())
+            elif event.key == pygame.K_DOWN:
+                self.snake.change_dir(self.snake.get_south())
+            elif event.key == pygame.K_RIGHT:
+                self.snake.change_dir(self.snake.get_east())
+            elif event.key == pygame.K_LEFT:
+                self.snake.change_dir(self.snake.get_west())
+            if event.key == pygame.K_SPACE:
+                self.alive = True
+                self.win = False
+                self.__init__()
+            self.load_world()
+            self.counter = 0
+    
     def load_world(self):
-
         self.screen.fill(self.background)
         self.die_event()
         self.win_event()
@@ -129,49 +138,7 @@ class Game():
 
     def update_world(self, array):
         self.world = array
-    
-    def keyboard_events(self, key_array = []):
-        key_counter = key_array.copy()
-        keys = pygame.key.get_pressed()
-
-        if len(key_counter) < 1:
-            key_counter.append(self.snake.direction)
-
-        actual_key = key_counter[len(key_counter)-1]
-
-        if keys[pygame.K_UP] and (self.snake.get_north() != self.snake.direction) and (self.snake.get_north() != actual_key):
-            key_counter.append(self.snake.get_north())
-
-        elif keys[pygame.K_DOWN] and (self.snake.get_south() != self.snake.direction) and (self.snake.get_south() != actual_key):
-            key_counter.append(self.snake.get_south())
-
-        elif keys[pygame.K_RIGHT] and (self.snake.get_east() != self.snake.direction) and (self.snake.get_east() != actual_key):
-            key_counter.append(self.snake.get_east())
-
-        elif keys[pygame.K_LEFT] and (self.snake.get_west() != self.snake.direction) and (self.snake.get_west() != actual_key):
-            key_counter.append(self.snake.get_west())
-
-        elif keys[pygame.K_SPACE]:
-            self.alive = True
-            self.win = False
-            self.__init__()
-
-
-        return key_counter
-    
-    def keyboard_buffer(self, keys_arr = []):
-        keys = keys_arr.copy()
-        if keys[0] == self.snake.get_north():
-            self.snake.change_dir(self.snake.get_north())
-        elif keys[0] == self.snake.get_south():
-            self.snake.change_dir(self.snake.get_south())
-        elif keys[0] == self.snake.get_east():
-            self.snake.change_dir(self.snake.get_east())
-        elif keys[0] == self.snake.get_west():
-            self.snake.change_dir(self.snake.get_west())
-        keys.pop(0)
-        return keys
-    
+        
     def eat_event(self):
         if self.snake.get_head_position() != self.food.get_food_position():
             self.world.insert_snake(self.snake.body)
